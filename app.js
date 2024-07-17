95d475678839e886cd41e627a0c56f89b66481e2
faraz9993 <farazansari9993@gmail.com>
2024-07-17 12:05:37 +0530

    Add Node.js application code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Kubernetes!');
 });
 
 app.listen(PORT, () => {
